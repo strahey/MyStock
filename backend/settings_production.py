73c76de638d9f363d18f03a75e158b95f2851b62
Scott Trahey <scott_trahey@yahoo.com>
2026-03-18 12:57:21 -0400

    fix admin access on local network

--- backend/settings_production.py
+++ backend/settings_production.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
